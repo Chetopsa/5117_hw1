2b7cf6f6648013d6531ef516c99a9f23698261d2
Chetopsa <125150503+Chetopsa@users.noreply.github.com>
2025-09-18 08:14:00 -0500

    hi

---
+++ README.md
@@ -0,0 +1,2 @@
+# 5117 Homework1
+## Cheston Opsasnick (opsas002)
--- app.py
+++ app.py
@@ -1,1 +1,29 @@
-print("hellow world")
+from flask import Flask, render_template, request, redirect, url_for
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/survey')
+def survey():
+    return render_template('survey.html')
+
+@app.route('/decline')
+def decline():
+    return render_template('decline.html')
+
+@app.route('/thanks')
+def thanks():
+    return render_template('thanks.html')
+
+@app.route('/api/results', methods=['POST'])
+def results():
+    pass
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port=5000, debug=True)
